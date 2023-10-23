9834a438d7b65d56b34879db5360882a1bc31c65
RaigZ <ragirzebari123@gmail.com>
2023-10-23 14:41:52 -0700

    Insert records into local_data_base

--- source/tools/eeg.py
+++ source/tools/eeg.py
@@ -6,13 +6,17 @@ from flask import g
 from tools.logging import logger   
 
 #doing all this a the "module level" in "Demo" server mode it will work fine :)
+from db_con import get_db_instance, get_db
+
+eeg_db = get_db()
+eeg_cur = eeg_db.cursor()
 
 def on_sensor_state_changed(sensor, state):
     logger.debug('Sensor {0} is {1}'.format(sensor.Name, state))
 
 def on_brain_bit_signal_data_received(sensor, data):
+    eeg_cur.execute("INSERT INTO brain (movieID, data) VALUES (?, ?)", (0, data))
     logger.debug(data)
-    g.db[]
 
 logger.debug("Create Headband Scanner")
 gl_scanner = Scanner([SensorFamily.SensorLEBrainBit])
