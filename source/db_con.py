56bdf713b915286e2ba2669b7df0b3d9d6636e06
Reese Hodge <146276759+ReeseHodge@users.noreply.github.com>
2023-11-05 17:22:33 -0800

    Update db_con.py
    capitalized create table

--- source/db_con.py
+++ source/db_con.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     for r in cur.fetchall():
         print(r)
 
-    cur.execute("create table brain (id int,movieID string(255), data string);")
+    cur.execute("CREATE TABLE brain (id int,movieID string(255), data string);")
     db.commit()
 
 
