d944fc6b1c606c46d94d2cb920a9d2c30e549929
TheFourthLugia <146274270+TheFourthLugia@users.noreply.github.com>
2023-11-08 14:18:50 -0800

    Update db_con.py

--- source/db_con.py
+++ source/db_con.py
@@ -8,11 +8,11 @@ import sqlite3
 def get_db():
     #Postgres
     #return psycopg2.connect(host="localhost", dbname="authme" , user="loki", password="4prez")
-    return sqlite3.connect("local_data_base")
+    return sqlite3.connect("local_data_base.db")
 
 def get_db_instance():  
     db  = get_db()
-    cur  = db.cursor( )
+    cur  = db.cursor()
 
     return db, cur 
 
@@ -25,8 +25,6 @@ if __name__ == "__main__":
     for r in cur.fetchall():
         print(r)
 
-    cur.execute("CREATE TABLE brain (id INTEGER PRIMARY KEY, movieID TEXT(255), data TEXT);")
-  
     db.commit()
 
 
