b5b10e870f1ca78d1e4d95ce9ea9b30fbb166f3e
Sepul014 <92600381+Sepul014@users.noreply.github.com>
2023-12-09 15:46:45 -0800

    Update app.py

--- source/app.py
+++ source/app.py
@@ -1,13 +1,14 @@
 from flask import Flask,render_template,request, redirect, url_for, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 import jwt
-
+import math
 import sys
 import datetime
 import bcrypt
 import traceback
-
-from tools.eeg import get_head_band_sensor_object
+import json
+#from tools.eeg import get_head_band_sensor_object
+from tools.eeg import simulate_brain_bit_signal_data, on_brain_bit_signal_data_received
 
 
 from db_con import get_db_instance, get_db
@@ -33,8 +34,9 @@ def init_new_env():
     if 'db' not in g:
         g.db = get_db()
 
-    if 'hb' not in g:
-        g.hb = get_head_band_sensor_object()
+    if 'hb' not in g:#
+        g.hb = None
+      #  g.hb = get_head_band_sensor_object()#
 
     #g.secrets = get_secrets()
     #g.sms_client = get_sms_client()
@@ -68,8 +70,6 @@ def exec_secure_proc(proc_name):
 
     return resp
 
-
-
 @app.route("/open_api/<proc_name>",methods=['GET', 'POST'])
 def exec_proc(proc_name):
     logger.debug(f"Call to {proc_name}")
@@ -92,6 +92,50 @@ def exec_proc(proc_name):
     return resp
 
 
+@app.route("/simulate_eeg_data")
+def simulate_eeg_data():
+    try:
+        simulated_data = simulate_brain_bit_signal_data()
+        
+        on_brain_bit_signal_data_received(None, simulated_data)
+        
+        return json_response(data={"message": "Simulated EEG data generated and stored successfully"})
+    except Exception as e:
+        logger.error(f"Error simulating EEG data: {e}")
+        return json_response(status_=500, data={"error": str(e)})
+
+
+def compute_average_euclidean_distance():
+    db, cursor = get_db_instance()
+    cursor.execute("SELECT data FROM brain")
+    rows = cursor.fetchall()
+
+    max_index = len(rows) - (len(rows) % 2) #if odd ignores last data set in table
+    total_distance = 0
+
+    for i in range(max_index // 2):
+        data1 = json.loads(rows[i][0])
+        data2 = json.loads(rows[max_index // 2 + i][0])
+
+        distance = math.sqrt(
+            (data1['O1'] - data2['O1']) ** 2 +
+            (data1['O2'] - data2['O2']) ** 2 +
+            (data1['T3'] - data2['T3']) ** 2 +
+            (data1['T4'] - data2['T4']) ** 2
+        )
+        total_distance += distance
+
+    average_distance = total_distance / (max_index // 2)
+    return average_distance
+
+@app.route("/average_euclidean_distance")
+def average_euclidean_distance():
+    try:
+        average_distance = compute_average_euclidean_distance()
+        return json_response(data={"Average_Euclidean_Distance": average_distance})
+    except Exception as e:
+        logger.error(f"Error computing average Euclidean distance: {e}")
+        return json_response(status_=500, data={"error": str(e)})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
-
