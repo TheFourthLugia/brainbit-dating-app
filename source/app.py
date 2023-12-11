9bf97971c7487f1dc922da56285cd6d2c4ee1a04
Sepul014 <92600381+Sepul014@users.noreply.github.com>
2023-12-10 17:56:31 -0800

    Update app.py

--- source/app.py
+++ source/app.py
@@ -6,7 +6,8 @@ import sys
 import datetime
 import bcrypt
 import traceback
-
+import math
+import json
 from tools.eeg import get_head_band_sensor_object
 
 
@@ -91,6 +92,37 @@ def exec_proc(proc_name):
 
     return resp
 
+def compute_average_euclidean_distance():
+    db, cursor = get_db_instance()
+    cursor.execute("SELECT data FROM brain")
+    rows = cursor.fetchall()
+
+    max_index = len(rows) - (len(rows) % 2) #if odd ignores last data set in table
+    total_distance = 0
+
+    for i in range(max_index // 2):
+        data1 = json.loads(rows[i][0])
+        data2 = json.loads(rows[max_index // 2 + i][0])
+
+        distance = math.sqrt(
+            (data1['O1'] - data2['O1']) ** 2 +
+            (data1['O2'] - data2['O2']) ** 2 +
+            (data1['T3'] - data2['T3']) ** 2 +
+            (data1['T4'] - data2['T4']) ** 2
+        )
+        total_distance += distance
+
+    average_distance = total_distance / (max_index // 2)
+    return average_distance
+
+@app.route("/average_euclidean_distance")
+def average_euclidean_distance():
+    try:
+        average_distance = compute_average_euclidean_distance()
+        return json_response(data={"Average_Euclidean_Distance": average_distance})
+    except Exception as e:
+        logger.error(f"Error computing average Euclidean distance: {e}")
+        return json_response(status_=500, data={"error": str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
